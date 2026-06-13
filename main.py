b10c5fce5682336925341ef62ac21522559e0ad9
Winkesa <winfreywekesa148@gmail.com>
2026-06-13 17:39:36 +0300

    import files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import functions from task_manager.task_utils package
-from task_utils import add_task, mark_task_as_complete,view_pending_tasks,calculate_progress
+from task_utils import (add_task, mark_task_as_complete,view_pending_tasks,calculate_progress)
 
 # Define the main function
 def main():
