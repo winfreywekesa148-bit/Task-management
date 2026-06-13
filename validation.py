f20169bb90cb358def6f22083b7324238b3b7f59
Winkesa <winfreywekesa148@gmail.com>
2026-06-13 18:16:42 +0300

    validation error

--- validation.py
+++ validation.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    None
+    if len(title) < 2:
+        raise ValueError("Title must be at least 2 characters long.")
+    return True
     
 def validate_task_description(description):
-    None    
+    if not description:
+        raise TypeError("Description can't be empty") 
     
 def validate_due_date(due_date):
-    None
+    try:
+        datetime.strptime(due_date)
+        return True
+    except TypeError:
+        raise TypeError("Enter correct date")
