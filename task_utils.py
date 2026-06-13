bcf35ae717652f4db7b41e20fb99fef3525e6448
Winkesa <winfreywekesa148@gmail.com>
2026-06-13 17:58:53 +0300

    changes

--- task_utils.py
+++ task_utils.py
@@ -5,13 +5,25 @@ from task_manager.validation import validate_task_title,validate_task_descriptio
 tasks = []
 
 # Implement add_task function
-def add_task(title, description, due_date):
-    None
+def add_task():
+    title = input("Enter title: ")
+    description = input("Enter description: ")
+    due_date = input("Enter due_date: ")
+
+    task = {
+        "title": "Groceries",
+        "description": "Shop at Market Basket for food", 
+        "due_date": "2024-06-26",
+        "completed": True
+    }
+
+    tasks.append(task)
+
     print("Task added successfully!")
     
 # Implement mark_task_as_complete function
 def mark_task_as_complete(index, tasks=tasks):
-    None
+    
     print("Task marked as complete!")
     
 # Implement view_pending_tasks function
