afb3fc83ba0db44ccc03d66e25889af6d0012ab9
Winkesa <winfreywekesa148@gmail.com>
2026-06-13 15:49:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Import functions from task_manager.task_utils package
+None
+
+# Define the main function
+def main():
+    while True:
+        print("Task Management System")
+        print("1. Add Task")
+        print("2. Mark Task as Complete")
+        print("3. View Pending Tasks")
+        print("4. View Progress")
+        print("5. Exit")
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == "1":
+            None
+        None
+        None
+        None
+        elif choice == "5":
+        print("Exiting the program...")
+        break
+        else:
+        print("Invalid choice. Please try again.")
+        
+if __name__ == "__main__":
+    main()
